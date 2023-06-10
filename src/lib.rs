c2c177a7eb890ed07dbc964ae653c688cf655f37
astral4 <88992929+astral4@users.noreply.github.com>
2023-06-09 23:39:24 -0400

    Initialize project

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+//! # fsbex
+//!
+//! Library for extracting audio from FMOD sound banks
+
+#![warn(clippy::pedantic, future_incompatible)]
+#![deny(
+    let_underscore_drop,
+    macro_use_extern_crate,
+    meta_variable_misuse,
+    missing_abi,
+    missing_debug_implementations,
+    missing_docs,
+    non_ascii_idents,
+    nonstandard_style,
+    noop_method_call,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts,
+    unreachable_pub,
+    unsafe_op_in_unsafe_fn,
+    unused,
+    unused_crate_dependencies,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_macro_rules,
+    unused_qualifications,
+    unused_results,
+    unused_tuple_struct_fields
+)]
+#![cfg_attr(not(test), no_std)]
