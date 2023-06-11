6f6ea09746241827ba041a64058a2c800a0751c3
astral4 <88992929+astral4@users.noreply.github.com>
2023-06-10 22:57:10 -0400

    set "unused" lint group to warn

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! `fsbex` is a library for extracting audio from FMOD sound banks. Only FSB version 5 is supported for now.
 
-#![warn(clippy::pedantic, future_incompatible)]
+#![warn(clippy::pedantic, future_incompatible, unused)]
 #![deny(
     let_underscore_drop,
     macro_use_extern_crate,
@@ -18,7 +18,6 @@
     trivial_numeric_casts,
     unreachable_pub,
     unsafe_op_in_unsafe_fn,
-    unused,
     unused_crate_dependencies,
     unused_import_braces,
     unused_lifetimes,
