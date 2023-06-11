61d6dc31876c588bb982c9f084bc4173c41ec097
astral4 <88992929+astral4@users.noreply.github.com>
2023-06-10 23:39:17 -0400

    impl PartialEq for unit tests; update tests

--- src/parse.rs
+++ src/parse.rs
@@ -42,12 +42,14 @@ impl<R: Read> Reader<R> {
 type ParseResult<T> = Result<T, ParseError>;
 
 #[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq))]
 enum ParseError {
     Incomplete(Needed),
     Failure,
 }
 
 #[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq))]
 enum Needed {
     Unknown,
     Size(NonZeroUsize),
@@ -73,24 +75,35 @@ impl Error for ParseError {}
 
 #[cfg(test)]
 mod test {
-    use std::num::NonZeroUsize;
-
     use super::{Needed, ParseError, Reader};
+    use std::num::NonZeroUsize;
 
     #[test]
     fn parse_single_number() {
         let data = b"\x00\x00\x00\x00\x00\x00";
         let mut reader = Reader::new(&data[..]);
-        assert_eq!(reader.le_u32().unwrap(), 0);
+        assert_eq!(reader.le_u32(), Ok(0));
     }
 
     #[test]
     fn parse_multiple_numbers() {
         let data = b"\x11\x00\x00\x00\x34\x12\x00\x00\x66\x66\x66\x66\xFF\xFF\xFF\xFF";
         let mut reader = Reader::new(&data[..]);
-        assert_eq!(reader.le_u32().unwrap(), 17);
-        assert_eq!(reader.le_u32().unwrap(), 4660);
-        assert_eq!(reader.le_u32().unwrap(), 1_717_986_918);
-        assert_eq!(reader.le_u32().unwrap(), u32::MAX);
+        assert_eq!(reader.le_u32(), Ok(17));
+        assert_eq!(reader.le_u32(), Ok(4660));
+        assert_eq!(reader.le_u32(), Ok(1_717_986_918));
+        assert_eq!(reader.le_u32(), Ok(u32::MAX));
+    }
+
+    #[test]
+    fn handle_incomplete_data() {
+        let data = b"\x00\x00";
+        let mut reader = Reader::new(&data[..]);
+        assert_eq!(
+            reader.le_u32(),
+            Err(ParseError::Incomplete(Needed::Size(
+                NonZeroUsize::new(2).unwrap()
+            )))
+        );
     }
 }
