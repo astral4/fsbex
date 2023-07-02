d23af9d61d7dbaa402914ddba34ea6ef0f2e6a59
astral4 <88992929+astral4@users.noreply.github.com>
2023-07-02 18:05:03 -0400

    add lazy stream processing

--- src/bank.rs
+++ src/bank.rs
@@ -1,10 +1,10 @@
 use std::{
     error::Error,
     fmt::{Display, Formatter, Result as FmtResult},
-    io::Read,
+    io::{Read, Write},
 };
 
-use crate::header::{error::HeaderError, Header};
+use crate::header::{error::HeaderError, Codec, Header, StreamInfo};
 use crate::read::Reader;
 
 struct Bank<R: Read> {
@@ -17,6 +17,35 @@ impl<R: Read> Bank<R> {
         let mut read = Reader::new(source);
         let header = Header::parse(&mut read)?;
         Ok(Self { header, read })
+    }
+
+    fn process_streams<F, E>(mut self, f: F) -> Result<(), ProcessError<E>>
+    where
+        F: Fn(LazyStream<'_, R>) -> Result<(), E>,
+    {
+        for (info, index) in self.header.stream_info.into_iter().zip(0..) {
+            f(LazyStream {
+                index,
+                info,
+                codec: self.header.codec,
+                reader: &mut self.read,
+            })
+            .map_err(|e| ProcessError::new(index, e))?;
+        }
+        Ok(())
+    }
+}
+
+struct LazyStream<'bank, R: Read> {
+    index: u32,
+    info: StreamInfo,
+    codec: Codec,
+    reader: &'bank mut Reader<R>,
+}
+
+impl<'bank, R: Read> LazyStream<'bank, R> {
+    fn write<W: Write>(sink: W) -> Result<(), EncodeError> {
+        todo!()
     }
 }
 
@@ -44,3 +73,33 @@ impl Error for DecodeError {
         self.inner.source()
     }
 }
+
+#[derive(Debug)]
+struct ProcessError<E> {
+    index: u32,
+    source: E,
+}
+
+impl<E> ProcessError<E> {
+    fn new(index: u32, source: E) -> Self {
+        Self { index, source }
+    }
+}
+
+impl<E> Display for ProcessError<E> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        f.write_str(&format!("failed to process stream at index {}", self.index))
+    }
+}
+
+impl<E: 'static + Error> Error for ProcessError<E> {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.source)
+    }
+}
+
+struct EncodeError {
+    source: EncodeErrorSource,
+}
+
+enum EncodeErrorSource {}
