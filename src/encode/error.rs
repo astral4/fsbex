400db36c4e9d168b72271d32d83bb31e8dba92dc
astral4 <88992929+astral4@users.noreply.github.com>
2023-07-02 22:06:26 -0400

    update EncodeError

--- src/encode/error.rs
+++ src/encode/error.rs
@@ -1,5 +1,66 @@
+use crate::read::ReadError;
+use std::{
+    error::Error,
+    fmt::{Display, Formatter, Result as FmtResult},
+};
+use vorbis_rs::VorbisError;
+
+#[derive(Debug)]
 pub(crate) struct EncodeError {
-    source: EncodeErrorSource,
+    kind: EncodeErrorKind,
+    source: Option<EncodeErrorSource>,
 }
 
-enum EncodeErrorSource {}
+#[derive(Debug)]
+pub(super) enum EncodeErrorKind {
+    Vorbis,
+}
+
+#[derive(Debug)]
+enum EncodeErrorSource {
+    Read(ReadError),
+    Vorbis(VorbisError),
+}
+
+impl EncodeError {
+    pub(super) fn new(kind: EncodeErrorKind) -> Self {
+        Self { kind, source: None }
+    }
+
+    pub(super) fn factory(kind: EncodeErrorKind) -> impl FnOnce(ReadError) -> Self {
+        |source| Self {
+            kind,
+            source: Some(EncodeErrorSource::Read(source)),
+        }
+    }
+
+    pub(super) fn from_vorbis(source: VorbisError) -> Self {
+        Self {
+            kind: EncodeErrorKind::Vorbis,
+            source: Some(EncodeErrorSource::Vorbis(source)),
+        }
+    }
+}
+
+impl Display for EncodeError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        f.write_str(&format!(
+            "an error occurred while encoding a {} stream",
+            match &self.kind {
+                EncodeErrorKind::Vorbis => "Vorbis",
+            }
+        ))
+    }
+}
+
+impl Error for EncodeError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match &self.source {
+            Some(source) => match source {
+                EncodeErrorSource::Read(e) => Some(e),
+                EncodeErrorSource::Vorbis(e) => Some(e),
+            },
+            None => None,
+        }
+    }
+}
