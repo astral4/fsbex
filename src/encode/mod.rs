31e877999e4c972b9d2557e4fdeef38f818fa148
astral4 <88992929+astral4@users.noreply.github.com>
2023-07-05 15:17:12 -0400

    finish PCM stream encoding; update PcmError source

--- src/encode/mod.rs
+++ src/encode/mod.rs
@@ -7,6 +7,8 @@ mod pcm;
 mod vorbis;
 mod vorbis_lookup;
 
+use pcm::Order;
+
 pub(crate) fn encode<R: Read, W: Write>(
     format: AudioFormat,
     info: &StreamInfo,
@@ -14,11 +16,21 @@ pub(crate) fn encode<R: Read, W: Write>(
     sink: W,
 ) -> Result<(), error::EncodeError> {
     match format {
-        AudioFormat::Pcm8 => pcm::encode::<_, _, 1, true>(info, source, sink)?,
-        AudioFormat::Pcm16 => pcm::encode::<_, _, 2, true>(info, source, sink)?,
-        AudioFormat::Pcm24 => pcm::encode::<_, _, 3, true>(info, source, sink)?,
-        AudioFormat::Pcm32 => pcm::encode::<_, _, 4, true>(info, source, sink)?,
-        AudioFormat::PcmFloat => pcm::encode::<_, _, 4, false>(info, source, sink)?,
+        AudioFormat::Pcm8 => {
+            pcm::encode::<_, _, 1, 1, true>(Order::LittleEndian, info, source, sink)?;
+        }
+        AudioFormat::Pcm16 => {
+            pcm::encode::<_, _, 2, 2, true>(Order::LittleEndian, info, source, sink)?;
+        }
+        AudioFormat::Pcm24 => {
+            pcm::encode::<_, _, 3, 3, true>(Order::LittleEndian, info, source, sink)?;
+        }
+        AudioFormat::Pcm32 => {
+            pcm::encode::<_, _, 4, 4, true>(Order::LittleEndian, info, source, sink)?;
+        }
+        AudioFormat::PcmFloat => {
+            pcm::encode::<_, _, 4, 4, false>(Order::LittleEndian, info, source, sink)?;
+        }
         AudioFormat::Vorbis => vorbis::encode(info, source, sink)?,
         _ => todo!(),
     }
--- src/encode/pcm.rs
+++ src/encode/pcm.rs
@@ -1,15 +1,27 @@
-use crate::{header::StreamInfo, read::Reader};
+use crate::{
+    header::StreamInfo,
+    read::{ReadError, Reader},
+};
 use std::{
     error::Error,
     fmt::{Display, Formatter, Result as FmtResult},
-    io::{Error as IoError, Read, Write},
+    io::{copy, Error as IoError, Read, Write},
 };
 
-pub(super) fn encode<R: Read, W: Write, const BYTE_DEPTH: u16, const IS_INT: bool>(
+pub(super) fn encode<
+    R: Read,
+    W: Write,
+    const BYTE_DEPTH: u16,
+    const BYTE_DEPTH_USIZE: usize,
+    const IS_INT: bool,
+>(
+    order: Order,
     info: &StreamInfo,
     source: &mut Reader<R>,
     sink: W,
 ) -> Result<(), PcmError> {
+    debug_assert_eq!(BYTE_DEPTH as usize, BYTE_DEPTH_USIZE);
+
     let mut sink = sink;
 
     write_header::<_, BYTE_DEPTH, IS_INT>(
@@ -18,9 +30,33 @@ pub(super) fn encode<R: Read, W: Write, const BYTE_DEPTH: u16, const IS_INT: boo
         info.sample_rate.into(),
         &mut sink,
     )
-    .map_err(PcmError::factory(PcmErrorKind::CreateHeader))?;
+    .map_err(PcmError::from_io(PcmErrorKind::CreateHeader))?;
 
-    todo!()
+    let start_pos = source.position();
+    let stream_size = u32::from(info.size) as usize;
+
+    if BYTE_DEPTH == 1 || order == Order::LittleEndian {
+        return copy(&mut source.limit(stream_size), &mut sink)
+            .map(|_| ())
+            .map_err(PcmError::from_io(PcmErrorKind::EncodeStream));
+    }
+
+    while source.position() - start_pos < stream_size {
+        let mut sample = source
+            .take_const::<BYTE_DEPTH_USIZE>()
+            .map_err(PcmError::from_read(PcmErrorKind::DecodeSample))?;
+
+        if IS_INT {
+            sample = uint_to_int(sample);
+        } else {
+            sample.reverse();
+        }
+
+        sink.write_all(sample.as_slice())
+            .map_err(PcmError::from_io(PcmErrorKind::EncodeSample))?;
+    }
+
+    Ok(())
 }
 
 fn write_header<W: Write, const BYTE_DEPTH: u16, const IS_INT: bool>(
@@ -50,6 +86,12 @@ fn write_header<W: Write, const BYTE_DEPTH: u16, const IS_INT: bool>(
     Ok(())
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub(super) enum Order {
+    LittleEndian,
+    BigEndian,
+}
+
 const MASK: u8 = 0b1000_0000;
 
 fn uint_to_int<const SIZE: usize>(bytes: [u8; SIZE]) -> [u8; SIZE] {
@@ -61,31 +103,56 @@ fn uint_to_int<const SIZE: usize>(bytes: [u8; SIZE]) -> [u8; SIZE] {
 #[derive(Debug)]
 pub(crate) struct PcmError {
     kind: PcmErrorKind,
-    source: IoError,
+    source: PcmErrorSource,
 }
 
 #[derive(Debug)]
 enum PcmErrorKind {
     CreateHeader,
+    EncodeStream,
+    DecodeSample,
+    EncodeSample,
+}
+
+#[derive(Debug)]
+enum PcmErrorSource {
+    Io(IoError),
+    Read(ReadError),
 }
 
 impl PcmError {
-    fn factory(kind: PcmErrorKind) -> impl FnOnce(IoError) -> Self {
-        |source| Self { kind, source }
+    fn from_io(kind: PcmErrorKind) -> impl FnOnce(IoError) -> Self {
+        |source| Self {
+            kind,
+            source: PcmErrorSource::Io(source),
+        }
+    }
+
+    fn from_read(kind: PcmErrorKind) -> impl FnOnce(ReadError) -> Self {
+        |source| Self {
+            kind,
+            source: PcmErrorSource::Read(source),
+        }
     }
 }
 
 impl Display for PcmError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         f.write_str(match self.kind {
-            PcmErrorKind::CreateHeader => "failed to encode PCM stream header",
+            PcmErrorKind::CreateHeader => "failed to encode file header",
+            PcmErrorKind::EncodeStream => "failed to encode full PCM stream",
+            PcmErrorKind::DecodeSample => "failed to decode sample from PCM stream",
+            PcmErrorKind::EncodeSample => "failed to encode sample",
         })
     }
 }
 
 impl Error for PcmError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(&self.source)
+        match &self.source {
+            PcmErrorSource::Io(e) => Some(e),
+            PcmErrorSource::Read(e) => Some(e),
+        }
     }
 }
 
