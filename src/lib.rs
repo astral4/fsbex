c5c6ad2be6e8d277fc612f21b08fa02e5f231b0e
astral4 <88992929+astral4@users.noreply.github.com>
2023-07-08 21:44:19 -0400

    update readme and root-level docs page

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,56 @@
 //! # fsbex
 //!
 //! `fsbex` is a library for extracting audio from FMOD sound banks. Only FSB version 5 is supported for now.
+//!
+//! ## Example
+//!
+//! Parsing a sound bank, then writing streams to files:
+//!
+//! ```ignore
+//! use fsbex::{Bank, AudioFormat};
+//! use std::{
+//!     error::Error,
+//!     io::{BufReader, BufWriter},
+//!     fs::File,
+//! };
+//!
+//! fn main() -> Result<(), Box<dyn Error>> {
+//!     // open file for reading sound bank
+//!     let file = BufReader::new(File::open("example.fsb")?);
+//!     let bank = Bank::new(file)?;
+//!
+//!     // report number of streams contained within the sound bank
+//!     println!("{} streams within this sound bank", bank.num_streams());
+//!
+//!     // check stream audio format
+//!     if bank.format() != AudioFormat::Vorbis {
+//!         return Err("expected Vorbis format".into());
+//!     }
+//!
+//!     // iterate over streams
+//!     for (index, stream) in bank.into_iter().enumerate() {
+//!         // check stream name
+//!         let file_name = if let Some(name) = stream.name() {
+//!             format!("{name}.ogg")
+//!         } else {
+//!             format!("stream_{index}.ogg")
+//!         };
+//!
+//!         // write stream data to file
+//!         let output_file = BufWriter::new(File::create(file_name)?);
+//!         stream.write(output_file)?;
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ## Supported formats
+//!
+//! `fsbex` supports encoding stream data for the following formats:
+//! - PCM (8, 16, 24, 32-bit integer)
+//! - PCM (32-bit float)
+//! - Vorbis
 
 #![warn(clippy::pedantic, future_incompatible, unused)]
 #![deny(
