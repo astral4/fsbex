66564185468be1b3d61c332d950a52fab3e20d43
astral4 <88992929+astral4@users.noreply.github.com>
2023-12-25 19:57:21 -0500

    move lint configuration to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -52,32 +52,6 @@
 //! - PCM (32-bit float)
 //! - Vorbis
 
-#![warn(clippy::pedantic, future_incompatible, unused)]
-#![deny(
-    let_underscore_drop,
-    macro_use_extern_crate,
-    meta_variable_misuse,
-    missing_abi,
-    missing_debug_implementations,
-    missing_docs,
-    non_ascii_idents,
-    nonstandard_style,
-    noop_method_call,
-    rust_2018_idioms,
-    trivial_casts,
-    trivial_numeric_casts,
-    unreachable_pub,
-    unsafe_op_in_unsafe_fn,
-    unused_crate_dependencies,
-    unused_import_braces,
-    unused_lifetimes,
-    unused_macro_rules,
-    unused_qualifications,
-    unused_results,
-    unused_tuple_struct_fields
-)]
-#![allow(clippy::module_name_repetitions)]
-
 mod bank;
 pub mod encode;
 mod header;
